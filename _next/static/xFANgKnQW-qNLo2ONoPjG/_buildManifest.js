4614ce0c94f8691a92deafae06c6cbef57327c84
viethoang234-se <viethoang234-se@users.noreply.github.com>
2026-06-30 01:17:22 +0000

    deploy: 22eddb80ac1baadb344d0c9bcf7018064cd2028b

---
+++ _next/static/xFANgKnQW-qNLo2ONoPjG/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/haa-future-spark//_next/:path+",
+        "destination": "/haa-future-spark/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
